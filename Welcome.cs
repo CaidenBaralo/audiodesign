cc771139a3aaae1ba8d6135328a8efdb9ae96489
CaidenBaralo <72476503+CaidenBaralo@users.noreply.github.com>
2020-10-06 20:30:56 -0400

    Update Welcome.cs

--- Welcome.cs
+++ Welcome.cs
@@ -8,26 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Trial1
+namespace Trial1 //This is the title of the program, "Trial 1"
 {
-    public partial class Welcome : Form
+    public partial class Welcome : Form //This is the title of the form, "Welcome"
     {
         public Welcome()
         {
-            InitializeComponent();
+            InitializeComponent(); //This locates the necessary code
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e) //This activates Form1 or the Welcome Form
         {
 
         }
 
-        private void Label1_Click(object sender, EventArgs e)
+        private void Label1_Click(object sender, EventArgs e) //Generates the "Welcome, click next to continue" 
         {
 
         }
 
-        private void Next_Click(object sender, EventArgs e)
+        private void Next_Click(object sender, EventArgs e) //This means when the user clicks, this form will be hidden (close), and the second form "Sysselect" will open
         {
             this.Hide();
             SysSelect f2 = new SysSelect();
