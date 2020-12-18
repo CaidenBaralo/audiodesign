74d3568b3b123cde52d86da84ac578ed9bd7aab3
CaidenBaralo <72476503+CaidenBaralo@users.noreply.github.com>
2020-12-18 10:45:17 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Num7 = new System.Windows.Forms.Button();
+            this.Num4 = new System.Windows.Forms.Button();
+            this.Num0 = new System.Windows.Forms.Button();
+            this.Num8 = new System.Windows.Forms.Button();
+            this.Num5 = new System.Windows.Forms.Button();
+            this.Num2 = new System.Windows.Forms.Button();
+            this.OpEquals = new System.Windows.Forms.Button();
+            this.Num9 = new System.Windows.Forms.Button();
+            this.Num6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.OpPlus = new System.Windows.Forms.Button();
+            this.OpMinus = new System.Windows.Forms.Button();
+            this.OpMultiply = new System.Windows.Forms.Button();
+            this.OpDivide = new System.Windows.Forms.Button();
+            this.Num1 = new System.Windows.Forms.Button();
+            this.Num3 = new System.Windows.Forms.Button();
+            this.NumDot = new System.Windows.Forms.Button();
+            this.OpClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Num7
+            // 
+            this.Num7.Location = new System.Drawing.Point(13, 89);
+            this.Num7.Name = "Num7";
+            this.Num7.Size = new System.Drawing.Size(137, 76);
+            this.Num7.TabIndex = 1;
+            this.Num7.Text = "7";
+            this.Num7.UseVisualStyleBackColor = true;
+            this.Num7.Click += new System.EventHandler(this.Num7_Click);
+            // 
+            // Num4
+            // 
+            this.Num4.Location = new System.Drawing.Point(12, 172);
+            this.Num4.Name = "Num4";
+            this.Num4.Size = new System.Drawing.Size(138, 68);
+            this.Num4.TabIndex = 2;
+            this.Num4.Text = "4";
+            this.Num4.UseVisualStyleBackColor = true;
+            this.Num4.Click += new System.EventHandler(this.Num4_Click);
+            // 
+            // Num0
+            // 
+            this.Num0.Location = new System.Drawing.Point(157, 13);
+            this.Num0.Name = "Num0";
+            this.Num0.Size = new System.Drawing.Size(130, 69);
+            this.Num0.TabIndex = 5;
+            this.Num0.Text = "0";
+            this.Num0.UseVisualStyleBackColor = true;
+            this.Num0.Click += new System.EventHandler(this.Num0_Click);
+            // 
+            // Num8
+            // 
+            this.Num8.Location = new System.Drawing.Point(157, 89);
+            this.Num8.Name = "Num8";
+            this.Num8.Size = new System.Drawing.Size(130, 76);
+            this.Num8.TabIndex = 6;
+            this.Num8.Text = "8";
+            this.Num8.UseVisualStyleBackColor = true;
+            this.Num8.Click += new System.EventHandler(this.Num8_Click);
+            // 
+            // Num5
+            // 
+            this.Num5.Location = new System.Drawing.Point(157, 172);
+            this.Num5.Name = "Num5";
+            this.Num5.Size = new System.Drawing.Size(130, 68);
+            this.Num5.TabIndex = 7;
+            this.Num5.Text = "5";
+            this.Num5.UseVisualStyleBackColor = true;
+            this.Num5.Click += new System.EventHandler(this.Num5_Click);
+            // 
+            // Num2
+            // 
+            this.Num2.Location = new System.Drawing.Point(157, 247);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(130, 75);
+            this.Num2.TabIndex = 8;
+            this.Num2.Text = "2";
+            this.Num2.UseVisualStyleBackColor = true;
+            this.Num2.Click += new System.EventHandler(this.Num2_Click);
+            // 
+            // OpEquals
+            // 
+            this.OpEquals.Location = new System.Drawing.Point(294, 13);
+            this.OpEquals.Name = "OpEquals";
+            this.OpEquals.Size = new System.Drawing.Size(132, 69);
+            this.OpEquals.TabIndex = 9;
+            this.OpEquals.Text = "=";
+            this.OpEquals.UseVisualStyleBackColor = true;
+            this.OpEquals.Click += new System.EventHandler(this.OpEquals_Click);
+            // 
+            // Num9
+            // 
+            this.Num9.Location = new System.Drawing.Point(294, 89);
+            this.Num9.Name = "Num9";
+            this.Num9.Size = new System.Drawing.Size(132, 76);
+            this.Num9.TabIndex = 10;
+            this.Num9.Text = "9";
+            this.Num9.UseVisualStyleBackColor = true;
+            this.Num9.Click += new System.EventHandler(this.Num9_Click);
+            // 
+            // Num6
+            // 
+            this.Num6.Location = new System.Drawing.Point(294, 172);
+            this.Num6.Name = "Num6";
+            this.Num6.Size = new System.Drawing.Size(132, 68);
+            this.Num6.TabIndex = 11;
+            this.Num6.Text = "6";
+            this.Num6.UseVisualStyleBackColor = true;
+            this.Num6.Click += new System.EventHandler(this.Num6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 445);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(556, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // OpPlus
+            // 
+            this.OpPlus.Location = new System.Drawing.Point(433, 248);
+            this.OpPlus.Name = "OpPlus";
+            this.OpPlus.Size = new System.Drawing.Size(136, 75);
+            this.OpPlus.TabIndex = 14;
+            this.OpPlus.Text = "+";
+            this.OpPlus.UseVisualStyleBackColor = true;
+            this.OpPlus.Click += new System.EventHandler(this.OpPlus_Click);
+            // 
+            // OpMinus
+            // 
+            this.OpMinus.Location = new System.Drawing.Point(433, 172);
+            this.OpMinus.Name = "OpMinus";
+            this.OpMinus.Size = new System.Drawing.Size(136, 68);
+            this.OpMinus.TabIndex = 15;
+            this.OpMinus.Text = "-";
+            this.OpMinus.UseVisualStyleBackColor = true;
+            this.OpMinus.Click += new System.EventHandler(this.OpMinus_Click);
+            // 
+            // OpMultiply
+            // 
+            this.OpMultiply.Location = new System.Drawing.Point(433, 89);
+            this.OpMultiply.Name = "OpMultiply";
+            this.OpMultiply.Size = new System.Drawing.Size(136, 76);
+            this.OpMultiply.TabIndex = 16;
+            this.OpMultiply.Text = "*";
+            this.OpMultiply.UseVisualStyleBackColor = true;
+            this.OpMultiply.Click += new System.EventHandler(this.OpMultiply_Click);
+            // 
+            // OpDivide
+            // 
+            this.OpDivide.Location = new System.Drawing.Point(433, 13);
+            this.OpDivide.Name = "OpDivide";
+            this.OpDivide.Size = new System.Drawing.Size(136, 69);
+            this.OpDivide.TabIndex = 17;
+            this.OpDivide.Text = "/";
+            this.OpDivide.UseVisualStyleBackColor = true;
+            this.OpDivide.Click += new System.EventHandler(this.OpDivide_Click);
+            // 
+            // Num1
+            // 
+            this.Num1.Location = new System.Drawing.Point(13, 247);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(137, 75);
+            this.Num1.TabIndex = 18;
+            this.Num1.Text = "1";
+            this.Num1.UseVisualStyleBackColor = true;
+            this.Num1.Click += new System.EventHandler(this.Num1_Click);
+            // 
+            // Num3
+            // 
+            this.Num3.Location = new System.Drawing.Point(294, 248);
+            this.Num3.Name = "Num3";
+            this.Num3.Size = new System.Drawing.Size(132, 74);
+            this.Num3.TabIndex = 19;
+            this.Num3.Text = "3";
+            this.Num3.UseVisualStyleBackColor = true;
+            this.Num3.Click += new System.EventHandler(this.Num3_Click);
+            // 
+            // NumDot
+            // 
+            this.NumDot.Location = new System.Drawing.Point(13, 13);
+            this.NumDot.Name = "NumDot";
+            this.NumDot.Size = new System.Drawing.Size(137, 69);
+            this.NumDot.TabIndex = 20;
+            this.NumDot.Text = ".";
+            this.NumDot.UseVisualStyleBackColor = true;
+            this.NumDot.Click += new System.EventHandler(this.NumDot_Click);
+            // 
+            // OpClear
+            // 
+            this.OpClear.Location = new System.Drawing.Point(13, 329);
+            this.OpClear.Name = "OpClear";
+            this.OpClear.Size = new System.Drawing.Size(556, 110);
+            this.OpClear.TabIndex = 21;
+            this.OpClear.Text = "Clear";
+            this.OpClear.UseVisualStyleBackColor = true;
+            this.OpClear.Click += new System.EventHandler(this.OpClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 497);
+            this.Controls.Add(this.OpClear);
+            this.Controls.Add(this.NumDot);
+            this.Controls.Add(this.Num3);
+            this.Controls.Add(this.Num1);
+            this.Controls.Add(this.OpDivide);
+            this.Controls.Add(this.OpMultiply);
+            this.Controls.Add(this.OpMinus);
+            this.Controls.Add(this.OpPlus);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Num6);
+            this.Controls.Add(this.Num9);
+            this.Controls.Add(this.OpEquals);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.Num5);
+            this.Controls.Add(this.Num8);
+            this.Controls.Add(this.Num0);
+            this.Controls.Add(this.Num4);
+            this.Controls.Add(this.Num7);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Num7;
+        private System.Windows.Forms.Button Num4;
+        private System.Windows.Forms.Button Num0;
+        private System.Windows.Forms.Button Num8;
+        private System.Windows.Forms.Button Num5;
+        private System.Windows.Forms.Button Num2;
+        private System.Windows.Forms.Button OpEquals;
+        private System.Windows.Forms.Button Num9;
+        private System.Windows.Forms.Button Num6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button OpPlus;
+        private System.Windows.Forms.Button OpMinus;
+        private System.Windows.Forms.Button OpMultiply;
+        private System.Windows.Forms.Button OpDivide;
+        private System.Windows.Forms.Button Num1;
+        private System.Windows.Forms.Button Num3;
+        private System.Windows.Forms.Button NumDot;
+        private System.Windows.Forms.Button OpClear;
+    }
+}
+
